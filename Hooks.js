6cf8de3f687e17623c8f7e2a28eb02532d22eec6
Benjamin Petry <benjamin@bpetry.de>
2020-06-25 23:32:48 +0200

    Corrected some useIntervall Hook Issues

--- Hooks.js
+++ Hooks.js
@@ -15,8 +15,8 @@ export function useInterval(callback, delay) {
       savedCallback.current();
     }
     if (delay !== null) {
-      let id = setInterval(tick, delay);
+      const id = setInterval(tick, delay);
       return () => clearInterval(id);
     }
-  }, [delay]);
+  }, [callback, delay]);
 }
